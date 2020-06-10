d88a918fcdc65a49bf8cec661de7cebc0d134f91
sandos123 <sanjotithakare@gmail.com>
2020-06-10 14:58:13 -0700

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,3 +1,19 @@
-window.onload = function() {
-  window.alert("Hello World");
+function cookieConsent() {
+    var allow = window.confirm("allow cookies?");
+    if (allow) { 
+        //set cookie or call salesforce api
+        var d = new Date();
+        d.setTime(d.getTime() + (365*24*60*60*1000)); // 1 year
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = "MyTestCookie" + "=" + "true" + ";" 
+          + expires 
+          + ";path=/" //+ $A.get("$Site.coreUrlPrefix")
+          + ";domain=" + $A.get("$Site.absCoreUrl");
+    }
 }
+
+window.onload = function() {
+  cookieConsent();
+}
+
+
